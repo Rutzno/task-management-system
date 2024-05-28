afa91f2946a87053edf36532555284dfb18862ea
Rutzno <d.rutzno@gmail.com>
2024-05-28 19:30:22 +0000

    initial commit

---
+++ src/main/java/com/diarpy/taskmanagementsystem/MyUser.java
@@ -0,0 +1,58 @@
+package com.diarpy.taskmanagementsystem;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+
+/**
+ * @author Mack_TB
+ * @since 18/05/2024
+ * @version 1.0.1
+ */
+
+@Entity
+public class MyUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @NotBlank(message = "Email is mandatory")
+    @Email(message = "Email invalid")
+    @Column(unique = true)
+    private String email;
+    @NotBlank(message = "Password is mandatory")
+    @Pattern(regexp = ".{6,}", message = "Password must be at least 6 characters long")
+//    @Size(min = 6, message = "Password must be at least 6 characters long")
+    private String password;
+    private String authority;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+}
---
+++ src/main/java/com/diarpy/taskmanagementsystem/TaskManagementSystemApplication.java
@@ -0,0 +1,19 @@
+package com.diarpy.taskmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Mack_TB
+ * @since 18/05/2024
+ * @version 1.0.1
+ */
+
+@SpringBootApplication
+public class TaskManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskManagementSystemApplication.class, args);
+    }
+
+}
