0c783be9f0c392745c6073e6434afc861ece9c17
Rutzno <d.rutzno@gmail.com>
2024-05-30 20:08:55 +0000

    stage 3

--- src/main/java/com/diarpy/taskmanagementsystem/SecurityConfig.java
+++ src/main/java/com/diarpy/taskmanagementsystem/SecurityConfig.java
@@ -11,7 +11,6 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
