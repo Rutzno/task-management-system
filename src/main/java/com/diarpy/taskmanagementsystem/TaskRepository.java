4bf58ddeafb9e765df44e4fe712532d9cc71641e
Rutzno <d.rutzno@gmail.com>
2024-05-30 19:23:26 +0000

    stage 2

---
+++ src/main/java/com/diarpy/taskmanagementsystem/TaskRepository.java
@@ -0,0 +1,17 @@
+package com.diarpy.taskmanagementsystem;
+
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+/**
+ * @author Mack_TB
+ * @since 18/05/2024
+ * @version 1.0.2
+ */
+
+public interface TaskRepository extends JpaRepository<Task, Long> {
+    List<Task> findAll(Sort sort);
+    List<Task> findByMyUser_Email(String email, Sort sort);
+}
