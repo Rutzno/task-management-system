a0a4c13ea753f4b65e784a9f459e5d96b15b654d
Rutzno <d.rutzno@gmail.com>
2024-05-30 20:22:36 +0000

    stage 4 - assigning tasks

--- src/main/java/com/diarpy/taskmanagementsystem/TaskController.java
+++ src/main/java/com/diarpy/taskmanagementsystem/TaskController.java
@@ -1,8 +1,13 @@
 package com.diarpy.taskmanagementsystem;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.validation.Errors;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -10,7 +15,7 @@ import java.util.List;
 /**
  * @author Mack_TB
  * @since 18/05/2024
- * @version 1.0.2
+ * @version 1.0.4
  */
 
 @RestController
@@ -23,13 +28,31 @@ public class TaskController {
     }
 
     @PostMapping
-    public Task postTask(@AuthenticationPrincipal UserDetails userDetails,
+    public Task postTask(Authentication authentication,
                          @RequestBody @Valid Task task) {
-        return taskService.save(userDetails, task);
+        return taskService.save(authentication, task);
     }
 
     @GetMapping
-    public List<Task> getTasks(@RequestParam(required = false) String author) {
-        return taskService.getTasks(author);
+    public List<Task> getTasks(@RequestParam(required = false) String author,
+                               @RequestParam(required = false) String assignee) {
+        return taskService.findAllTasks(author, assignee);
     }
+
+    @PutMapping("/{id}/assign")
+    public ResponseEntity<Task> updateTaskByAssignee(Authentication authentication,
+                                                     @PathVariable Long id,
+                                                     @RequestBody AssignTaskRequest assignTaskRequest) {
+        return taskService.updateByAssignee(authentication, id, assignTaskRequest.assignee());
+    }
+
+    @PutMapping("/{id}/status")
+    public ResponseEntity<Task> updateTaskByStatus(Authentication authentication,
+                                                   @PathVariable Long id,
+                                                   @RequestBody StatusTaskRequest statusTaskRequest) {
+        return taskService.updateByStatus(authentication, id, statusTaskRequest.status());
+    }
+
+    record AssignTaskRequest(String assignee) {}
+    record StatusTaskRequest(TaskStatus status) {}
 }
