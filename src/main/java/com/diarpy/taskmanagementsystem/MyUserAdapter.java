34dcdb270c300a39f0059ca9f03e9bdfcda5742a
Rutzno <d.rutzno@gmail.com>
2024-05-30 18:56:15 +0000

    add MyUserAdapter.java

---
+++ src/main/java/com/diarpy/taskmanagementsystem/MyUserAdapter.java
@@ -0,0 +1,57 @@
+package com.diarpy.taskmanagementsystem;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * @author Mack_TB
+ * @since 18/05/2024
+ * @version 1.0.1
+ */
+
+public class MyUserAdapter implements UserDetails {
+    private final MyUser myUser;
+
+    public MyUserAdapter(MyUser myUser) {
+        this.myUser = myUser;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(myUser.getAuthority()));
+    }
+
+    @Override
+    public String getPassword() {
+        return myUser.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return myUser.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
