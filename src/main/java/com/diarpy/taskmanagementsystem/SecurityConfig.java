39e394f5f7ff4d3c55c843f3fca7303df3b638fb
Rutzno <d.rutzno@gmail.com>
2024-05-30 19:47:38 +0000

    stage 3

--- src/main/java/com/diarpy/taskmanagementsystem/SecurityConfig.java
+++ src/main/java/com/diarpy/taskmanagementsystem/SecurityConfig.java
@@ -1,20 +1,34 @@
 package com.diarpy.taskmanagementsystem;
 
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.security.KeyPair;
+import java.security.interfaces.RSAPublicKey;
+import java.util.UUID;
 
 /**
  * @author Mack_TB
  * @since 18/05/2024
- * @version 1.0.2
+ * @version 1.0.3
  */
 
 @Configuration
@@ -24,12 +38,14 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .httpBasic(Customizer.withDefaults()) // enable basic HTTP authentication
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt) // enabling jwt authentication
                 .authorizeHttpRequests(auth -> auth
-                                .requestMatchers(HttpMethod.POST,"/api/accounts").permitAll()
+                                .requestMatchers(HttpMethod.POST,"/api/accounts").anonymous()
                                 .requestMatchers("/error").permitAll() // expose the /error endpoint
                                 .requestMatchers("/actuator/shutdown").permitAll() // required for tests
                                 .requestMatchers("/h2-console/**").permitAll() // expose H2 console
 //                        .requestMatchers("/api/tasks/**").authenticated()
+//                        .requestMatchers(HttpMethod.POST, "/auth/token").authenticated()
                                 .anyRequest().authenticated()
                 )
                 .csrf(AbstractHttpConfigurer::disable) // allow modifying requests from tests
@@ -44,4 +60,28 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    JwtDecoder jwtDecoder(KeyPair keyPair) {
+        return NimbusJwtDecoder
+                .withPublicKey((RSAPublicKey) keyPair.getPublic())
+                .build();
+    }
+
+    @Bean
+    JWKSource<SecurityContext> jwkSource(KeyPair keyPair) {
+        RSAKey rsaKey = new RSAKey.Builder((RSAPublicKey) keyPair.getPublic())
+                .privateKey(keyPair.getPrivate())
+                .keyID(UUID.randomUUID().toString())
+                .build();
+        JWKSet jwkSet = new JWKSet(rsaKey);
+        return new ImmutableJWKSet<>(jwkSet);
+    }
+
+    @Bean
+    public JwtEncoder jwtEncoder(JWKSource<SecurityContext> jwkSource) {
+        return new NimbusJwtEncoder(jwkSource);
+    }
+
+
 }
