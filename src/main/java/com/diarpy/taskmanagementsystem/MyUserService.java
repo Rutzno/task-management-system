e8a76bc2c6cdca33e997598b42a56d7dd0fd416c
Rutzno <d.rutzno@gmail.com>
2024-05-30 19:03:26 +0000

    add MyUserService.java and SecurityConfig.java

---
+++ src/main/java/com/diarpy/taskmanagementsystem/MyUserService.java
@@ -0,0 +1,39 @@
+package com.diarpy.taskmanagementsystem;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.Errors;
+
+/**
+ * @author Mack_TB
+ * @since 18/05/2024
+ * @version 1.0.1
+ */
+
+@Service
+public class MyUserService {
+    private final MyUserRepository myUserRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public MyUserService(MyUserRepository myUserRepository, PasswordEncoder passwordEncoder) {
+        this.myUserRepository = myUserRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public ResponseEntity<?> register(MyUser myUser, Errors errors) {
+        if (myUserRepository.findByEmailIgnoreCase(myUser.getEmail()).isPresent()) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        }
+        if (errors.hasErrors()) {
+            return ResponseEntity.badRequest().build();
+        }
+        myUser.setPassword(passwordEncoder.encode(myUser.getPassword()));
+        myUser.setAuthority("ROLE_USER");
+        myUserRepository.save(myUser);
+        return ResponseEntity.ok().build();
+    }
+}
---
+++ src/main/java/com/diarpy/taskmanagementsystem/SecurityConfig.java
@@ -0,0 +1,46 @@
+package com.diarpy.taskmanagementsystem;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * @author Mack_TB
+ * @since 18/05/2024
+ * @version 1.0.1
+ */
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .httpBasic(Customizer.withDefaults()) // enable basic HTTP authentication
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.POST,"/api/accounts").permitAll()
+                        .requestMatchers("/error").permitAll() // expose the /error endpoint
+                        .requestMatchers("/actuator/shutdown").permitAll() // required for tests
+                        .requestMatchers("/h2-console/**").permitAll() // expose H2 console
+                        .anyRequest().authenticated()
+                )
+                .csrf(AbstractHttpConfigurer::disable) // allow modifying requests from tests
+//                .headers(cfg -> cfg.frameOptions().disable()) // disable X-Frame-Options to unblock it
+                .sessionManagement(sessions ->
+                        sessions.sessionCreationPolicy(SessionCreationPolicy.STATELESS) // no session
+                )
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
