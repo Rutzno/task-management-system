43a4989d6bf5b987a4bc84624fb381158efc78d1
Rutzno <d.rutzno@gmail.com>
2024-05-30 19:08:25 +0000

    add ApplicationController.java

---
+++ src/main/java/com/diarpy/taskmanagementsystem/ApplicationController.java
@@ -0,0 +1,39 @@
+package com.diarpy.taskmanagementsystem;
+
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @author Mack_TB
+ * @since 18/05/2024
+ * @version 1.0.1
+ */
+
+@RestController
+@RequestMapping("/api")
+public class ApplicationController {
+    private final MyUserService myUserService;
+
+    @Autowired
+    public ApplicationController(MyUserService myUserService, PasswordEncoder passwordEncoder) {
+        this.myUserService = myUserService;
+    }
+
+    @PostMapping(path = "/accounts")
+    public ResponseEntity<?> register(@RequestBody @Valid MyUser myUser, Errors errors) {
+        return myUserService.register(myUser, errors);
+    }
+
+    @GetMapping(path = "/tasks")
+    @ResponseStatus(code = HttpStatus.OK)
+    public void getTasks() {
+
+    }
+
+//    record RegistrationRequest(String email, String password) { }
+}
