69bc7fafe9d4f39df614700d6e8902754cbc2c24
Rutzno <d.rutzno@gmail.com>
2024-05-30 19:58:25 +0000

    stage 3

---
+++ src/main/java/com/diarpy/taskmanagementsystem/RsaKeysConfig.java
@@ -0,0 +1,29 @@
+package com.diarpy.taskmanagementsystem;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * @author Mack_TB
+ * @since 18/05/2024
+ * @version 1.0.3
+ */
+
+@Configuration
+public class RsaKeysConfig {
+
+    @Bean
+    public KeyPair generateRsaKeys() {
+        try {
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+            keyPairGenerator.initialize(2048);
+            return keyPairGenerator.generateKeyPair();
+        } catch (NoSuchAlgorithmException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+}
