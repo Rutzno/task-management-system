c6dc82bde8f6304d20004b751e7ef4122f93b2c1
Rutzno <d.rutzno@gmail.com>
2024-05-30 20:26:20 +0000

    stage 5

--- src/main/java/com/diarpy/taskmanagementsystem/TaskRepository.java
+++ src/main/java/com/diarpy/taskmanagementsystem/TaskRepository.java
@@ -8,11 +8,13 @@ import java.util.List;
 /**
  * @author Mack_TB
  * @since 18/05/2024
- * @version 1.0.4
+ * @version 1.0.5
  */
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
     List<Task> findAll(Sort sort);
     List<Task> findByMyUser_Email(String email, Sort sort);
     //    boolean existsById(Long id);
+     /*@Query("SELECT DISTINCT t, t.comments.size AS total_comments FROM Task t LEFT JOIN FETCH t.comments")
+    List<Task> findAllByAssigneeAndAuthor(String assignee, String author);*/
 }
